7e96e2a6a16b4fdcb8b69bdcdc51a6ebaf21990e
Philip Braunen <pbraunen@gmail.com>
2021-08-01 21:25:03 +0200

    ADd slug and token methods to Str model

--- app/Models/Str.php
+++ app/Models/Str.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
-class Str {
+class Str
+{
     public static function sanitize(string $input)
     {
         return htmlspecialchars(trim($input), ENT_QUOTES, 'utf-8');
+    }
+
+    public static function slug(string $string)
+    {
+        $disallowedCharacters = '/[^\-\s\pN\pL]+/u';
+        $spacesDuplicateHyphens = '/[\-\s]+/';
+
+        $slug = mb_strtolower($string, 'UTF-8');
+        $slug = preg_replace($disallowedCharacters, '', $slug);
+        $slug = preg_replace($spacesDuplicateHyphens, '-', $slug);
+
+        return $slug;
+    }
+
+    public static function token(int $length = 16)
+    {
+        return bin2hex(random_bytes($length));
     }
 }
