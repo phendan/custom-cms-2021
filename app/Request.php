9437b813d419af5766cac1371347e965f59dfab9
Philip Braunen <pbraunen@gmail.com>
2021-08-01 21:24:30 +0200

    Extend Request class

--- app/Request.php
+++ app/Request.php
@@ -11,6 +11,7 @@ class Request {
     {
         $this->getParams = $getParams;
         $this->postParams = $_POST;
+        $this->headers = $this->parseHeaders();
     }
 
     public function hasInput()
@@ -23,6 +24,20 @@ class Request {
         return $this->sanitize(array_merge($this->getParams, $this->postParams));
     }
 
+    public function hasFile(?string $fileName)
+    {
+        if (isset($fileName)) {
+            return isset($_FILES[$fileName]);
+        }
+
+        return isset($_FILES);
+    }
+
+    public function getFiles()
+    {
+        return $_FILES;
+    }
+
     public function only(array|string ...$params)
     {
         if (is_array($params[0])) {
@@ -32,6 +47,37 @@ class Request {
         return array_filter($this->getInput(), function($value, $param) use($params) {
             return in_array($param, $params);
         }, ARRAY_FILTER_USE_BOTH);
+    }
+
+    public function getMethod()
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
+    private function parseHeaders()
+    {
+        $rawHeaders = array_filter($_SERVER, fn($element) => str_starts_with($element, 'HTTP_'));
+        $headers = [];
+
+        foreach ($rawHeaders as $key => $header) {
+            $keyFragments = explode('_', str_replace('HTTP_', '', $key));
+            $keyFragments = array_map(fn($fragment) => ucfirst(strtolower($fragment)), $keyFragments);
+            $key = implode('-', $keyFragments);
+            $output[$key] = $header;
+        }
+
+        return $headers;
+    }
+
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    public function expectsJson()
+    {
+        $headers = $this->getHeaders();
+        return in_array('Accept', $headers) && $headers['Accept'] === 'application/json';
     }
 
     public function sanitize($input)
