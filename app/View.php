3a8f7e26dc67d2f3b568e557fd3ccfa259749c44
phendan <10546560+phendan@users.noreply.github.com>
2021-08-01 16:58:37 +0200

    Add basic output sanitization

--- app/View.php
+++ app/View.php
@@ -2,9 +2,24 @@
 
 namespace App;
 
+use App\Config;
+use App\Models\Session;
+use App\Models\Str;
+
 class View {
     public function render($view, array $data = [])
     {
+        $data['session'] = Session::class;
+        $data['root'] = Config::get('root');
+
+        $data = array_map(function ($element) {
+            if (is_string($element)) {
+                return Str::sanitize($element);
+            }
+
+            return $element;
+        }, $data);
+
         extract($data);
 
         require_once '../app/Views/partials/header.php';
